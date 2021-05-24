1503f53e2e364fff55089cf313ab419cc066e76c
Kaj Rolandsson <kajrolandsson@gmail.com>
2021-05-24 11:06:31 +0200

    introduce movement to aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,6 +32,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
 
@@ -82,6 +83,10 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
 
 
     def _update_screen(self):
